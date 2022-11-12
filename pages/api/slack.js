41b518fce8e34daf4dcaadda1d958794e71fc879
Ubirajara Neves <ubirajara.neves@usp.br>
2022-11-12 15:14:41 -0300

    Correção de erro para Slack

--- pages/api/slack.js
+++ pages/api/slack.js
@@ -1,71 +1,78 @@
 import config from '../../config.json';
 
-const getSlackInfo = () => {
-    const url = config.urlIndices;
-    const data = fetch(url).then((res) => res.json());
+const getSlackInfo = async () => {
+    try {
+        const url = config.urlIndices;
+        const res = await fetch(url);
+        const data = res.json();
 
-    const resposta = {
-        response_type: 'in_channel',
-        blocks: [
-            {
-                type: 'header',
-                text: {
-                    type: 'plain_text',
-                    text: 'Índices para reajuste',
-                    emoji: true,
+        const resposta = {
+            response_type: 'in_channel',
+            blocks: [
+                {
+                    type: 'header',
+                    text: {
+                        type: 'plain_text',
+                        text: 'Índices para reajuste',
+                        emoji: true,
+                    },
                 },
-            },
-            {
-                type: 'section',
-                text: {
-                    type: 'plain_text',
-                    text: '📈 Valores acumulados para 12 meses:',
-                    emoji: true,
+                {
+                    type: 'section',
+                    text: {
+                        type: 'plain_text',
+                        text: '📈 Valores acumulados para 12 meses:',
+                        emoji: true,
+                    },
                 },
-            },
-            {
-                type: 'section',
-                text: {
-                    type: 'mrkdwn',
-                    text: '- *IPCA:* ' + data.indices[0].valor + '%',
+                {
+                    type: 'section',
+                    text: {
+                        type: 'mrkdwn',
+                        text: '- *IPCA:* ' + data.indices[0].valor + '%',
+                    },
                 },
-            },
-            {
-                type: 'section',
-                text: {
-                    type: 'mrkdwn',
-                    text: '- *IGP-M:* ' + data.indices[1].valor + '%',
+                {
+                    type: 'section',
+                    text: {
+                        type: 'mrkdwn',
+                        text: '- *IGP-M:* ' + data.indices[1].valor + '%',
+                    },
                 },
-            },
-            {
-                type: 'divider',
-            },
-            {
-                type: 'section',
-                text: {
-                    type: 'mrkdwn',
-                    text: '*Fontes:* IBGE e FGV.',
+                {
+                    type: 'divider',
                 },
-            },
-            {
-                type: 'section',
-                text: {
-                    type: 'mrkdwn',
-                    text: '_(Este é um recurso experimental. Em caso de dúvida, procure seu líder!)_',
+                {
+                    type: 'section',
+                    text: {
+                        type: 'mrkdwn',
+                        text: '*Fontes:* IBGE e FGV.',
+                    },
                 },
-            },
-            {
-                type: 'section',
-                text: {
-                    type: 'mrkdwn',
-                    text:
-                        '_(Última atualização: ' + data.timestamp + ' (GMT))_',
+                {
+                    type: 'section',
+                    text: {
+                        type: 'mrkdwn',
+                        text: '_(Este é um recurso experimental. Em caso de dúvida, procure seu líder!)_',
+                    },
                 },
-            },
-        ],
-    };
+                {
+                    type: 'section',
+                    text: {
+                        type: 'mrkdwn',
+                        text:
+                            '_(Última atualização: ' +
+                            data.timestamp +
+                            ' (GMT))_',
+                    },
+                },
+            ],
+        };
 
-    return resposta;
+        return resposta;
+    } catch (err) {
+        throw new Error(err.message);
+    }
 };
 
 export default getSlackInfo;
