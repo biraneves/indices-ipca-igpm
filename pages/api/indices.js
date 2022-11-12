391b04dc5cb8de13090bf19b2ad9b6d3e49abcc6
Ubirajara Neves <ubirajara.neves@usp.br>
2022-11-12 14:40:04 -0300

    API com indices agora é interna.

---
+++ pages/api/indices.js
@@ -0,0 +1,62 @@
+import axios from 'axios';
+import cheerio from 'cheerio';
+import config from '../../config.json';
+
+const getIpcaAcumulado12 = async () => {
+    try {
+        const url = config.urlIpca;
+
+        const res = await axios.get(url);
+        const dados = {
+            titulo: 'ipca',
+            valor: parseFloat(res.data[1].V),
+        };
+
+        return dados;
+    } catch (err) {
+        throw new Error(err.message);
+    }
+};
+
+const getIgpmAcumulado12 = async () => {
+    try {
+        const url = config.urlIgpm;
+        const res = await axios.get(url);
+        const html = res.data;
+        const $ = cheerio.load(html);
+        const cards = $('p.card-indice-numero.text-center');
+
+        const indices = [];
+
+        cards.each((idx, el) => {
+            indices.push(parseFloat($(el).text().trim().replace(',', '.')));
+        });
+
+        const dados = {
+            titulo: 'igp-m',
+            valor: indices[1],
+        };
+
+        return dados;
+    } catch (err) {
+        throw new Error(err.message);
+    }
+};
+
+const indices = async (_request, response) => {
+    const res = {
+        indices: [],
+    };
+
+    res.indices.push(await getIpcaAcumulado12());
+    res.indices.push(await getIgpmAcumulado12());
+
+    response.setHeader(
+        'Cache-Control',
+        's-maxage=36000, stale-while-revalidate',
+    );
+
+    response.json(res);
+};
+
+export default indices;
